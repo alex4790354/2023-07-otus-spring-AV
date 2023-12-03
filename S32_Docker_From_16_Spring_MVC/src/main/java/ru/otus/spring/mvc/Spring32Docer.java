1bae0af572ac126db917a54c38a1b35199ee83c8
Alexander V. <9720560@gmail.com>
2023-12-03 17:27:55 +0300

    S32-Docker. Few changes.

--- S32_Docker_From_16_Spring_MVC/src/main/java/ru/otus/spring/mvc/Spring32Docer.java
+++ S32_Docker_From_16_Spring_MVC/src/main/java/ru/otus/spring/mvc/Spring32Docer.java
@@ -7,11 +7,11 @@ import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-public class Spring31Docer {
+public class Spring32Docer {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = SpringApplication.run(Spring31Docer.class);
+        ApplicationContext context = SpringApplication.run(Spring32Docer.class);
 
     }
 
